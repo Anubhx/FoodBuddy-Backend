2d8d24f6de48392e803cab5caf6fc6eb07c80039
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-05-11 23:15:37 +0530

    Refactor restaurantController.js to add validation for required fields

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,18 +2,31 @@ const Restaurant = require('../models/Restaurant');
 
 module.exports = {
     addRestaurant : async (req, res) => {
+
+        const {title, time,imageUrl, owner, code, logoUrl,coords} = req.body;
+        
+        if(!title || !time || !imageUrl || ! owner || !code || !logoUrl || !coords 
+            || !coords.latitude || ! coords.longitude || coords.address || !coords.title){
+                return res.status(400).json({status: false, message: "All fields are required"});
+            }
+
         try {
-            
+            const newRestaurant = new Restaurant(req.body);
+            await newRestaurant.save();
+            res.status(201).json({status: true, message: 'Restaurant created successfully',});
         } catch (error) {
             
         }
     },
 
     getRestaurantById : async (req, res) => {
+        const id = req.params.id;
         try {
-            
+            const restaurant = await Restaurant.findById(id);
+
+            res.status(200).json(restaurant);
         } catch (error) {
-            
+            res.status(500).json({status: false, message: error.message,});
         }
     },
 
